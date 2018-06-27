1da9ec7eb3f0103fb459cada4d8e4758822b9cc4
Yoloboss456 <sonicjosh.562@gmail.com>
2018-06-26 20:38:48 -0500

    Prettified the calc

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,28 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class Calc extends JPanel implements ActionListener{
 
     private JFrame frame;
     private JTextField txtfield, txtfield2;
     private JButton addbutton, multbutton, divbutton, subbutton;
-    private JPanel panel;
+    private JPanel panel, panel2, panel3;
     private String num1, num2;
     private JLabel result;
+    DecimalFormat df = new DecimalFormat("###.###");
 
     public void gui(){
 
         frame = new JFrame("Calculator");
         frame.pack();
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         panel = new JPanel();
+        panel2 = new JPanel();
+        panel3 = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel2.setLayout(new FlowLayout());
+        panel3.setLayout(new BorderLayout());
 
-        txtfield = new JTextField("First Number",20);
-        txtfield2 = new JTextField("Second Number", 20);
+        txtfield = new JTextField("",20);
+        txtfield2 = new JTextField("", 20);
         addbutton = new JButton("+");
         subbutton = new JButton("-");
         multbutton = new JButton("*");
@@ -34,15 +40,27 @@ public class Calc extends JPanel implements ActionListener{
         multbutton.addActionListener(this);
         divbutton.addActionListener(this);
 
-        panel.add(txtfield);
-        panel.add(txtfield2);
-        panel.add(addbutton);
-        panel.add(subbutton);
-        panel.add(multbutton);
-        panel.add(divbutton);
-        panel.add(result);
-        frame.setContentPane(panel);
+        Dimension size = new Dimension(50, 50);
+        addbutton.setPreferredSize(size);
+        subbutton.setPreferredSize(size);
+        multbutton.setPreferredSize(size);
+        divbutton.setPreferredSize(size);
 
+        panel.add(txtfield, "North");
+        panel.add(txtfield2, "South");
+        panel2.add(addbutton);
+        panel2.add(subbutton);
+        panel2.add(multbutton);
+        panel2.add(divbutton);
+        result.setFont(new Font("Serif", Font.BOLD, 50));
+        panel3.add(result, "West");
+
+        frame.getContentPane().add(panel, "North");
+        frame.getContentPane().add(panel2, "Center");
+        frame.getContentPane().add(panel3, "South");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public String GrabText(JTextField text){
@@ -54,29 +72,29 @@ public class Calc extends JPanel implements ActionListener{
             num1 = GrabText(txtfield);
             num2 = GrabText(txtfield2);
 
-            float answer = Float.parseFloat(num1) + Float.parseFloat(num2);
-            result.setText("" + Float.toString(answer));
+            double answer = Double.parseDouble(num1) + Double.parseDouble(num2);
+            result.setText("" + Double.toString(Double.parseDouble(df.format(answer))));
         }
         if(e.getSource() == subbutton) {
             num1 = GrabText(txtfield);
             num2 = GrabText(txtfield2);
 
-            float answer = Float.parseFloat(num1) - Float.parseFloat(num2);
-            result.setText("" + Float.toString(answer));
+            double answer = Double.parseDouble(num1) - Double.parseDouble(num2);
+            result.setText("" + Double.toString(Double.parseDouble(df.format(answer))));
         }
         if(e.getSource() == multbutton) {
             num1 = GrabText(txtfield);
             num2 = GrabText(txtfield2);
 
-            float answer = Float.parseFloat(num1) * Float.parseFloat(num2);
-            result.setText("" + Float.toString(answer));
+            double answer = Double.parseDouble(num1) * Double.parseDouble(num2);
+            result.setText("" + Double.toString(Double.parseDouble(df.format(answer))));
         }
         if(e.getSource() == divbutton) {
             num1 = GrabText(txtfield);
             num2 = GrabText(txtfield2);
 
-            float answer = Float.parseFloat(num1) / Float.parseFloat(num2);
-            result.setText("" + Float.toString(answer));
+            double answer = Double.parseDouble(num1) / Double.parseDouble(num2);
+            result.setText("" + Double.toString(Double.parseDouble(df.format(answer))));
         }
 
     }
